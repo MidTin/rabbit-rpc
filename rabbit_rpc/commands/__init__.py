ced717670ede3e1ae2adcc72cda5f37bc781ac8a
MidTin <midtin@gmail.com>
2018-05-07 15:19:05 +0800

    0.0.1 without doc

---
+++ rabbit_rpc/commands/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+import sys
+
+
+def get_commands():
+    from .worker import Worker
+    return {Worker.name: Worker()}
+
+
+class ManageUtility(object):
+
+    def __init__(self, argv=None):
+        self.argv = argv
+
+    def fetch_command(self, subcommand):
+        commands = get_commands()
+        try:
+            return commands[subcommand]
+        except KeyError:
+            pass
+
+    def execute(self):
+        parser = ArgumentParser()
+        parser.add_argument('worker', help='start a server worker')
+        # parser.add_argument('call', help='send remote call')
+
+        if len(self.argv) > 2:
+
+        try:
+            subcommand = self.argv[1]
+            command = self.fetch_command(subcommand)
+            if not command:
+                parser.print_help()
+                sys.stderr.write('Unkown subcommand: %s\n' % subcommand)
+                sys.exit(1)
+        except IndexError:
+            parser.print_help()
+            sys.exit()
+
+        try:
+            command = self.fetch_command(subcommand)
+            command.run_from_argv(*self.argv[2:])
+        except KeyboardInterrupt:
+            pass
---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    requires = f.readlines()
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    README = f.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='rabbit-rpc',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A simple rpc client/server library',
+    long_description=README,
+    author='midtin',
+    author_email='midtin@gmail.com',
+    url='https://github.com/MidTin/rabbit-rpc',
+    license='MIT',
+    install_requires=requires,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    entry_points={
+        'console_scripts': [
+            'rabbit_rpc=rabbit_rpc:main',
+        ]
+    }
+)
