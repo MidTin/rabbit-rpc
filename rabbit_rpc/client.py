7dc2b59dbcfbada991c2e00812191b439cdee969
MidTin <midtin@gmail.com>
2018-07-23 10:54:01 +0800

    improve client

--- rabbit_rpc/client.py
+++ rabbit_rpc/client.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import functools
 import logging
 import json
 import threading
@@ -17,18 +18,34 @@ class RPCClient(object):
 
     def __init__(self, amqp_url, exchange='default'):
         self._results = {}
-
-        parameters = pika.URLParameters(amqp_url)
-        self._connection = pika.BlockingConnection(parameters)
-
         self._exchange = exchange
-        self._channel = self._connection.channel()
-        ret = self._channel.queue_declare(exclusive=True, auto_delete=True)
-        self.callback_queue = ret.method.queue
 
-        t = threading.Thread(target=self._process_data_events)
+        self.url = amqp_url
+        self._closed = False
+
+        self.internal_lock = threading.Lock()
+
+        t = threading.Thread(target=self.connect)
         t.daemon = True
         t.start()
+
+    def reconnect(self):
+        if not self._closed:
+            self.connect()
+
+    def connect(self):
+        with self.internal_lock:
+            parameters = pika.URLParameters(self.url)
+            self._connection = pika.BlockingConnection(parameters)
+            self._channel = self._connection.channel()
+
+            ret = self._channel.queue_declare(exclusive=True, auto_delete=True)
+            self.callback_queue = ret.method.queue
+
+            self._channel.basic_consume(
+                self.on_response, no_ack=True, queue=self.callback_queue)
+
+        self._process_data_events()
 
     def _process_data_events(self):
         """Check for incoming data events.
@@ -36,11 +53,10 @@ class RPCClient(object):
         asynchronous requests.
         It is important that we lock the thread each time we check for events.
         """
-        self._channel.basic_consume(self.on_response, no_ack=True,
-                                    queue=self.callback_queue)
-        while True:
-            self._connection.process_data_events()
-            time.sleep(0.1)
+        while not self._closed:
+            with self.internal_lock:
+                self._connection.process_data_events()
+                time.sleep(0.1)
 
     def on_response(self, channel, basic_deliver, props, body):
         ret = json.loads(body)
@@ -52,29 +68,28 @@ class RPCClient(object):
     def get_response(self, correlation_id, timeout=None):
         stoploop = time.time() + timeout if timeout is not None else 0
 
-        self._connection.process_data_events()
         while stoploop > time.time() or timeout is None:
-            self._connection.process_data_events()
-
             if correlation_id in self._results:
                 return self._results.pop(correlation_id)
 
-            time.sleep(0.05)
+            time.sleep(0.1)
 
         raise RemoteCallTimeout()
 
     def publish_message(self, exchange, routing_key, body, headers=None):
         corr_id = str(uuid.uuid4())
 
-        self._channel.basic_publish(
-            exchange=exchange,
-            routing_key=routing_key,
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                headers=headers,
-                correlation_id=corr_id,
-            ),
-            body=json.dumps(body))
+        self._connection.add_timeout(
+            0, functools.partial(
+                self._channel.basic_publish,
+                exchange=exchange,
+                routing_key=routing_key,
+                properties=pika.BasicProperties(
+                    reply_to=self.callback_queue,
+                    headers=headers,
+                    correlation_id=corr_id,
+                ),
+                body=json.dumps(body)))
 
         return corr_id
 
@@ -108,11 +123,12 @@ class RPCClient(object):
                 'kwargs': kwargs,
             }
 
-            corr_id = self.publish_message(
-                exchange,
-                routing_key,
-                body=payload,
-                headers={'consumer_name': consumer_name})
+            with self.internal_lock:
+                corr_id = self.publish_message(
+                    exchange,
+                    routing_key,
+                    body=payload,
+                    headers={'consumer_name': consumer_name})
 
             logger.info('Sent remote call: %s', consumer_name)
             if not ignore_result:
